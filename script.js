5df68125b63ef6e1273f0ce83af8d646a402d354
Yeboah024 <yeboahantwi024@gmail.com>
2025-04-22 15:20:31 +0000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('navbar-toggle')[0];
-const navbarlinks = document.getElementsByClassName('navbar-links');
-toggleButton.addEventListener('click',function() {
-    for(var i=0; i<navbarlinks.length; i++)
-    navbarlinks[i].classList.toggle('active');
-});
